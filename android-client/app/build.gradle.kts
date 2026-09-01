92626a7f9110810554520975e3996f772f391719
judgeou <judgeou@vip.qq.com>
2026-09-01 10:49:33 +0800

    chore(android): bump version to 0.5.3

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "top.ozaoza.remoe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.5.2"
+        versionCode = 2
+        versionName = "0.5.3"
 
         ndk {
             abiFilters += "arm64-v8a"
